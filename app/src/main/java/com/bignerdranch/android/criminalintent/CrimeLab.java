ef9c2d2f3d26ece9db3b089214cf48268a3c9b6a
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-14 10:04:21 -0700

    Add CrimeLab getters and setters

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -2,8 +2,14 @@ package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
 
     public static CrimeLab get(Context context){
         if(sCrimeLab == null){
@@ -11,7 +17,23 @@ public class CrimeLab {
         }
         return sCrimeLab;
     }
+    //Private constructor for singleton
     private CrimeLab(Context context){
-        
+        mCrimes = new ArrayList<>(); //Infer list items type based on variable declaration
+    }
+
+    //getCrimes getter
+    public List<Crime> getCrimes(){
+        return mCrimes;
+    }
+
+    //Get specific crime by UUID
+    public Crime getCrime(UUID id){
+        for(Crime crime: mCrimes){  //Search for crime in mCrimes ArrayList
+            if(crime.getId().equals(id)){   //If UUIDs match, return crime
+                return crime;
+            }
+        }
+        return null;    //If no crimes match. return null
     }
 }
