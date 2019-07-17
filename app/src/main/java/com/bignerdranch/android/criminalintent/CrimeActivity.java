3de31946a3b763d9fd42a7cae0b21a31ae033101
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-17 10:40:28 -0700

    Implement newIntent() method in CrimeActivity that returns an Intent with crime UUID attached as extra

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -4,9 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
+
+    //String hold extra's key
+    public static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
+
+    /** Creates on explicit Intent object and attaches to it, as an extra, the
+     *  UUID of the crime who's details need to be accessed.
+     *
+     * @param packageContext activity calling this function (hosting activity) as Context object
+     * @param crimeId   UUID of crime to display details of
+     * @return          intent object
+     */
+    public static Intent newIntent(Context packageContext, UUID crimeId){
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
     @Override
     protected Fragment createFragment(){
         return new CrimeFragment();
