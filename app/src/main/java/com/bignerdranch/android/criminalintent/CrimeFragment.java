74aa565db3e2493fd78fb0ffaab6f1e4d95fb9e3
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-17 11:00:33 -0700

    Retreive extra and fetch corresponding crime in CrimeFragment. Update Title and CheckBox accordingly

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -13,6 +13,8 @@ import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.UUID;
+
 import static android.widget.CompoundButton.*;  //Imports all static members from CompoundButton type
 
 public class CrimeFragment extends Fragment {
@@ -24,8 +26,10 @@ public class CrimeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
-
+        //getIntent() returns Intent that was used to start activity
+        UUID crimeId = (UUID) getActivity().getIntent()
+                                           .getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
     @Override
@@ -33,6 +37,7 @@ public class CrimeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
         mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after){
@@ -55,6 +60,7 @@ public class CrimeFragment extends Fragment {
         mDateButton.setEnabled(false);  //Disabled, user cannot interact with it
 
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener(){
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
