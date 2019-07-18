fa9d958da6df690c1218daa4f2467a69a2ca730d
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-18 13:08:33 -0700

    Add newIntent() to CrimePagerActivity and extra for crime ID

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,26 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import java.util.List;
+import java.util.UUID;
 
 public class CrimePagerActivity extends AppCompatActivity {
+    private static final String EXTRA_CRIME_ID = "com.bignerdranch.criminalintent.crime_id";
 
     private ViewPager mViewPager;   //Pager object
     private List<Crime> mCrimes;   //List to store crimes from CrimeLab
+
+    public static Intent newIntent(Context packageContext, UUID crimeId){
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class); //Create new intent to this Activity
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);   //Put crimeId as extra on the intent
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);  //Set root view of activity and inflate
+
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID); //Get crimeId from extras
 
         mViewPager = (ViewPager) findViewById(R.id.crime_view_pager); //
 
