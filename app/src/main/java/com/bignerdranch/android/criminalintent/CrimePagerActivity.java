6b9400f5b945f97c452e961935c0a1bdef6a126b
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-18 12:11:26 -0700

    Create CrimePagerActivity and override onCreate(Bundle)

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+        
+    }
+}
