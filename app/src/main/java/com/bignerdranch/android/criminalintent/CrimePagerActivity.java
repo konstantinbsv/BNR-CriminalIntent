97ad06e442e6097f8ac7bf08bf0cff25467002a4
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-18 12:56:12 -0700

    Set up FragmentStatePagerAdapter and its FragmentManager

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -3,13 +3,39 @@ package com.bignerdranch.android.criminalintent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.List;
 
 public class CrimePagerActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;   //Pager object
+    private List<Crime> mCrimes;   //List to store crimes from CrimeLab
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime_pager);
-        
+        setContentView(R.layout.activity_crime_pager);  //Set root view of activity and inflate
+
+        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager); //
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager(); //Create FragmentManger object
+                                        //for interacting with fragments associated with this activity
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
     }
 }
