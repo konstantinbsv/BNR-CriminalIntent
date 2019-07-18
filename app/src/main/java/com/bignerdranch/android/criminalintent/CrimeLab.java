ea85bcdebc92657d67701a668220761a0d26e514
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-17 17:21:48 -0700

    Complete Challenge 10.2: Improve CrimeLab Performance, sol: implement LinkedHashMap for storing Crimes with their UUIDs as keys

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -3,13 +3,16 @@ package com.bignerdranch.android.criminalintent;
 import android.content.Context;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
-    private List<Crime> mCrimes;
+    //private List<Crime> mCrimes;
+    Map<UUID,Crime> mCrimes;
 
     //Singleton method for CrimeLab
     public static CrimeLab get(Context context){
@@ -20,29 +23,25 @@ public class CrimeLab {
     }
     //Private constructor for singleton
     private CrimeLab(Context context){
-        mCrimes = new ArrayList<>(); //Infer list items type based on variable declaration
+        mCrimes = new LinkedHashMap<>(); // Use Linked Hash Map for quick searching, insertion
+                                         // and deletion, while maintaining order of items
 
-        //Populate List<Crime> mCrimes with 100 test crimes
+        //Populate LinkedHasHMap<UUID,Crime> mCrimes with 100 test crimes
         for (int i = 0; i < 100; i++ ){
             Crime crime = new Crime();      //Create new Crime instance
             crime.setTitle("Crime #" + i);  //Set title with number
             crime.setSolved(i % 2 == 0);    //Set even as solved
-            mCrimes.add(crime);             //Append to mCrimes List
+            mCrimes.put(crime.getId(), crime);  //Put crime in LHM mCrimes with UUID as key
         }
     }
 
     //getCrimes getter
     public List<Crime> getCrimes(){
-        return mCrimes;
+        return new ArrayList<>(mCrimes.values());
     }
 
     //Get specific crime by UUID
     public Crime getCrime(UUID id){
-        for(Crime crime: mCrimes){  //Search for crime in mCrimes ArrayList
-            if(crime.getId().equals(id)){   //If UUIDs match, return crime
-                return crime;
-            }
-        }
-        return null;    //If no crimes match. return null
+        return mCrimes.get(id);
     }
 }
