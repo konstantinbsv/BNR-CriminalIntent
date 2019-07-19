bafdffc1c476399292b1b543a757b85bff60fbe2
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-18 21:46:09 -0700

    Inflate buttons in CrimePagerActivity and set OnClick listeners

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -3,6 +3,8 @@ package com.bignerdranch.android.criminalintent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -17,7 +19,10 @@ public class CrimePagerActivity extends AppCompatActivity {
     private static final String EXTRA_CRIME_ID = "com.bignerdranch.criminalintent.crime_id";
 
     private ViewPager mViewPager;   //Pager object
-    private List<Crime> mCrimes;   //List to store crimes from CrimeLab
+    private List<Crime> mCrimes;    //List to store crimes from CrimeLab
+
+    private Button mJumpToFirst;    //Jump to last and first cime buttons
+    private Button mJumpToLast;
 
     public static Intent newIntent(Context packageContext, UUID crimeId){
         Intent intent = new Intent(packageContext, CrimePagerActivity.class); //Create new intent to this Activity
@@ -35,8 +40,10 @@ public class CrimePagerActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.crime_view_pager); //
 
         mCrimes = CrimeLab.get(this).getCrimes();
-        FragmentManager fragmentManager = getSupportFragmentManager(); //Create FragmentManger object
+        final FragmentManager fragmentManager = getSupportFragmentManager(); //Create FragmentManger object
                                         //for interacting with fragments associated with this activity
+
+        //Create anonymous PagerAdapter and set it on ViewPager
         mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
             @Override
             public Fragment getItem(int position) {
@@ -50,11 +57,28 @@ public class CrimePagerActivity extends AppCompatActivity {
             }
         });
 
+
         for (int i = 0; i < mCrimes.size(); i++)
             if(mCrimes.get(i).getId().equals(crimeId)){
                 mViewPager.setCurrentItem(i);
                 break;
             }
+
+        mJumpToFirst = (Button) findViewById(R.id.jump_to_first_button);
+        mJumpToFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager.setCurrentItem(0);
+            }
+        });
+
+        mJumpToLast = (Button) findViewById(R.id.jump_to_last_button);
+        mJumpToLast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager.setCurrentItem(mCrimes.size() - 1);
+            }
+        });
 
     }
 }
