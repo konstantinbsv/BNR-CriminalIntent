31f0a1d16663427c0e9341ca6f3ccab0445bed2f
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-21 17:51:48 -0700

    Initialize DatePicker with date from arguments bundle

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -9,6 +9,7 @@ import android.widget.DatePicker;
 
 import androidx.fragment.app.DialogFragment;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class DatePickerFragment  extends DialogFragment {
@@ -28,8 +29,19 @@ public class DatePickerFragment  extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Date date = (Date) getArguments().getSerializable(ARG_DATE); // get Date from arguments budle
+
+        Calendar calendar = Calendar.getInstance(); // Gets a calendar using default timezone and locale
+        calendar.setTime(date); // set calendar with Date date
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         View v = LayoutInflater.from(getActivity()) // get LayoutInflater instance hooked up to the current context
                 .inflate(R.layout.dialog_date, null); // inflate DatePicker from specified xml
+
+        mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_picker); // return reference to first descendant view in hierarchy with given id
+        mDatePicker.init(year, month, day, null); // initialize DatePicker
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v) // configure dialog to display DatePicker b/en title and button(s) (sets custom view)
