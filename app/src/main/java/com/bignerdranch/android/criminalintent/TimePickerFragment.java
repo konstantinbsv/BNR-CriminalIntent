219d299365bf1e23f0e94a8b11378d86276dbbd6
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-23 09:51:19 -0700

    Create newInstance(date) that attaches date as a bundle argument to a new instance of TimePickerFragment and returns the fragment

--- app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.java
@@ -1,8 +1,11 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
 import android.widget.TimePicker;
 
 import androidx.fragment.app.DialogFragment;
+
+import java.util.Date;
 
 public class TimePickerFragment extends DialogFragment {
 
@@ -10,5 +13,14 @@ public class TimePickerFragment extends DialogFragment {
     public static final String ARG_TIME = "time";
 
     private TimePicker mTimePicker;
+
+    public static TimePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_TIME, date);
+
+        TimePickerFragment timeFragment = new TimePickerFragment();
+        timeFragment.setArguments(args);
+        return timeFragment;
+    }
 
 }
