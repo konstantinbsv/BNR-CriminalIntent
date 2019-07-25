6c71f46231c18f282569fd2ff280c129f17afcd7
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-25 10:38:43 -0700

    Create DatePickerActivity.newIntent(...) and pass date to DPF.newInstance(Date)

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerActivity.java
@@ -1,11 +1,25 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
+
 import androidx.fragment.app.Fragment;
+
+import java.util.Date;
 
 public class DatePickerActivity extends SingleFragmentActivity {
 
+    public static final String EXTRA_DATE = "com.bignerdranch.android.criminalintent.date";
+
+    public static Intent newIntent(Context packageContext, Date date) {
+        Intent intent = new Intent(packageContext, DatePickerActivity.class);
+        intent.putExtra(EXTRA_DATE, date);
+        return intent;
+    }
+
     @Override
     protected Fragment createFragment() {
-        return new DatePickerFragment();
+        Date date = (Date) getIntent().getSerializableExtra(EXTRA_DATE);
+        return DatePickerFragment.newInstance(date);
     }
 }
