76cc8504a758a4fb7cf7f759ba9d24e33fd69aca
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-25 09:36:23 -0700

    Override SingleFragmentActivity.createFragment() to host DatePickerFragment

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerActivity.java
@@ -1,4 +1,11 @@
 package com.bignerdranch.android.criminalintent;
 
+import androidx.fragment.app.Fragment;
+
 public class DatePickerActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new DatePickerFragment();
+    }
 }
