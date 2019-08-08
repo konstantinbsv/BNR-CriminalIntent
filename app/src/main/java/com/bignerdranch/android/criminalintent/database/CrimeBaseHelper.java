d66562da7196c29ce35c55c4e928ea4d3d75e695
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-08-07 18:04:47 -0700

    Create CrimeBaseHelper

--- app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
@@ -1,4 +1,24 @@
 package com.bignerdranch.android.criminalintent.database;
 
-public class CrimeBaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+    public static final int VERSION = 1;
+    public static final String DATABASE_NAME = "crimeBase.db";
+
+    public CrimeBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
 }
